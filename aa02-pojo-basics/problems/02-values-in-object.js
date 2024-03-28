81a6ebaf1ad517654485d5b8465413d99226d566
Erika <brandonerika96@gmail.com>
2024-03-28 15:05:14 -0400

    secondproblem done

--- aa02-pojo-basics/problems/02-values-in-object.js
+++ aa02-pojo-basics/problems/02-values-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
@@ -13,10 +13,21 @@ let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
 valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
 valuesInObject(foods); // => ["tart", "sour", "sweet"]
 ***********************************************************************/
+let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
 
 function valuesInObject(obj) {
-  // Your code here 
+
+  for (let key in obj) {
+    //console.log(Object.keys(obj))
+    console.log(obj[key])
+  }
+
+  // console.log(Object.values(obj))
 }
+
+valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
+valuesInObject(foods); // => ["tart", "sour", "sweet"]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuesInObject;
